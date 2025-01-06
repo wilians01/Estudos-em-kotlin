f466065e0d51cc51967a83f988a34fda44b2107b
wilians01 <wilians01100@gmail.com>
2025-01-06 17:02:39 -0300

    Algumas anotações

--- Estudos/src/Estudos/Anotacoes.kt
+++ Estudos/src/Estudos/Anotacoes.kt
@@ -27,8 +27,29 @@ fun main() {
     val resposta = mediaDePreco.toDouble()
     // se quiser converter um tipo coloque depois da variavel .to---- e o tipo da que você
 
-
     println(resposta)
+
+    //podemos no momento da declaração de um numero grande colocar _ para ficar melhor intendimento
+    val  numeroGrande =1_000_000
+
+    //NUMEROS
+    val byte: Byte = 8 // 8-bit
+    val short: Short = 16 // 16-bit
+    val age: Int = 31 // 32-bit
+    val long: Long = 11515114 // 64-bit
+
+    val numerosFlutuantes: Float = 54.56f // 32-bit
+    val numero3: Double = 31.90 //64-bit
+
+    // ALGUMAS FUNCÕES
+    // Para saber o tamanho de uma varial ou arry, String etc.....
+    val nome = "Carlos Pereira Nunnes"
+    println("O nome $nome tem ${nome.length} letras")
+    // Obs: para colocar uma Função dentro de println tem que colocar ${ } se não, não dá certo  
+
+
+
+
 
 
 
