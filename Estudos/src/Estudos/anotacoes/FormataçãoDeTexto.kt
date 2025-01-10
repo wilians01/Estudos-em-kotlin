c853040b9aadce8c053decbbbf87ce2c375e8ce6
wilians01 <wilians01100@gmail.com>
2025-01-09 21:50:36 -0300

    Atualizações

--- Estudos/src/Estudos/anotacoes/FormataçãoDeTexto.kt
+++ Estudos/src/Estudos/anotacoes/FormataçãoDeTexto.kt
@@ -6,12 +6,21 @@ fun main() {
     // frase vc coloca \n isso ira fazer o texto quebra uma linha.
 
     //Outro jeito mais legal de você fazer isso é dessa maneira
+
     val mensagem2 = """
         Olá meu amigo
         tudo bem!!!
         
     """.trimIndent()
-    println("$mensagem\n")
-    println(mensagem2)
+    //temos outros tipos de formato esse abaixo é mais para Execel
+    val mensagem3 = """
+        texto-1
+        texto-2
+        """.replaceIndent(";")// essa função ira colocar uma String na frente do seu textos formatados
+
+
+//    println("$mensagem\n")
+//    println(mensagem2)
+    println(mensagem3)
 
 }
