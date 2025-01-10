58d00b011ebe579a9233cd7842c290e70d0cb73f
wilians01 <wilians01100@gmail.com>
2025-01-10 17:38:38 -0300

    Atualizações

---
+++ Estudos/src/estudos/dio/controleDeFluxo/For.kt
@@ -0,0 +1,10 @@
+package estudos.dio.controleDeFluxo
+
+fun main() {
+    // Um exemplo de estrutura for
+    val sabores = listOf("chocolate","morango","abacaxi","uva")
+
+    for(sabor in sabores){
+        println("Temos bolo de $sabor")
+    }
+}
--- Estudos/src/estudos/dio/controleDeFluxo/When.kt
+++ Estudos/src/estudos/dio/controleDeFluxo/When.kt
@@ -1,7 +1,6 @@
 package estudos.dio.controleDeFluxo
 
 
-
 fun main() {
 
     cases("Olá")
@@ -11,19 +10,18 @@ fun main() {
     cases("Tudo ok")
 
 
-
 }
 
 // when pode ser usado para substituir estruturas switch case
- fun cases(obj: Any){ // esse tipo Any pode receber qualquer coisa
-    when(obj){
+fun cases(obj: Any) { // esse tipo Any pode receber qualquer coisa
+    when (obj) {
         1 -> println("Olá tudo bem")
         "Olá" -> println("como você vai")
         is Long -> println("Long")
         !is String -> println("Não é um String")
         else -> println("nenhumas das opções da função")
     }
- }
+}
 
 class MyClass
 
