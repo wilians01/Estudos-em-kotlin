b40514c9f96b0df18bb45dd26a6349f4e8cfab54
wilians01 <wilians01100@gmail.com>
2025-01-11 16:44:48 -0300

    Add estrutura Iteratores

--- Estudos/src/estudos/dio/controleDeFluxo/Iteratores.kt
+++ Estudos/src/estudos/dio/controleDeFluxo/Iteratores.kt
@@ -1,2 +1,19 @@
 package estudos.dio.controleDeFluxo
 
+class Aniamal(val nome: String)
+
+class Zoo(val animais: List<Aniamal>){
+
+    operator fun iterator(): Iterator<Aniamal>{
+        return animais.iterator()
+    }
+}
+
+fun main() {
+    val animais = listOf(Aniamal("cavalo"),Aniamal("leão"),Aniamal("jacare"))
+    val zoo = Zoo(animais)
+
+    for (animal in zoo){
+        println("Um ${animal.nome}")
+    }
+}
