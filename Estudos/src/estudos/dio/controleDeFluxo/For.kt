3ca180393030827482eca559b12632cc941763cd
wilians01 <wilians01100@gmail.com>
2025-01-11 16:43:30 -0300

    Add estrutura for, while, do while

--- Estudos/src/estudos/dio/controleDeFluxo/For.kt
+++ Estudos/src/estudos/dio/controleDeFluxo/For.kt
@@ -7,4 +7,18 @@ fun main() {
     for(sabor in sabores){
         println("Temos bolo de $sabor")
     }
+
+    var n1 = 0
+    var n2 = 0
+
+    while (n1 <5 ){//Um exemplo de estrutura de while
+        println("Parabéns")
+        n1++
+    }
+
+    do {//Um exemplo de estrutura de do while
+        println("Teste")
+        n2++
+    }while (n2 < 2)
+
 }
---
+++ Estudos/src/estudos/dio/controleDeFluxo/Iteratores.kt
@@ -0,0 +1,2 @@
+package estudos.dio.controleDeFluxo
+
