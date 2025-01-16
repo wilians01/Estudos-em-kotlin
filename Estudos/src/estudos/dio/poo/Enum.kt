930f84970ee29db8d36c56acb7329f9f1a450100
wilians01 <wilians01100@gmail.com>
2025-01-16 14:22:52 -0300

    Criação do arquivo e exemplos Enum

--- Estudos/src/estudos/dio/poo/Enum.kt
+++ Estudos/src/estudos/dio/poo/Enum.kt
@@ -1,2 +1,19 @@
 package estudos.dio.poo
 
+enum class State {
+    FACIL, NORMAL, DIFICIL  // 1 Define uma classe enum simples com três constantes enum.
+                            // O número de constantes é sempre finito e todas são distintas.
+}
+
+fun main() {
+    val state = State.NORMAL      // 2 Acessa uma constante enum por meio do nome da classe.
+
+    val message = when (state) {  // 3 Com enums, o compilador pode inferir se uma when-expressão
+                                  // é exaustiva, de modo que você não precisa do else-case.
+        State.FACIL -> "Modo Fácil"
+        State.NORMAL -> "Modo Normal "
+        State.DIFICIL -> "Modo Difícil"
+    }
+    println(message)
+    println(state)
+}
