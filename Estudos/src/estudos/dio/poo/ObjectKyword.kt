410d6b5012216a2c6dc708cb4ed021399d1889aa
wilians01 <wilians01100@gmail.com>
2025-01-16 15:24:14 -0300

    Atualização

--- Estudos/src/estudos/dio/poo/ObjectKyword.kt
+++ Estudos/src/estudos/dio/poo/ObjectKyword.kt
@@ -1,2 +1,8 @@
 package estudos.dio.poo
 
+//Palavra-chave do objeto
+//Classes e objetos em Kotlin funcionam da mesma forma que na maioria das
+//linguagens orientadas a objetos: uma classe é um blueprint, e um objeto
+//é uma instância de uma classe. Normalmente, você define uma classe e então cria múltiplas instâncias dessa classe:
+
+//Da uma olhada na documentação https://play.kotlinlang.org/byExample/03_special_classes/04_Object
