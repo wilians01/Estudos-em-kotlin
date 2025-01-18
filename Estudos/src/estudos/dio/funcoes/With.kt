68a8e80eeda34956da66041f9e0be5a92375835f
wilians01 <wilians01100@gmail.com>
2025-01-18 18:01:30 -0300

    Exemplo de fução de escopo With

--- Estudos/src/estudos/dio/funcoes/With.kt
+++ Estudos/src/estudos/dio/funcoes/With.kt
@@ -1,2 +1,23 @@
 package estudos.dio.funcoes
 
+// With é uma função não-extensiva que pode acessar membros de seu argumento de forma concisa:
+// você pode omitir o nome da instância ao se referir a seus membros.
+
+class Configuration(val host: String, val port: Int)
+
+fun main() {
+    val configuration = Configuration(host = "127.0.0.1 ", port = 9000)
+
+
+    with(configuration) {
+        println("$host:$port")
+    }
+    // outra alternativa
+    configuration.run {
+        println("$host $port")
+    }
+
+    // em vez de:
+    println("${configuration.host}:${configuration.port}")
+
+}
