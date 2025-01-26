2e37018e0e6816ef6f29ceb70f151efcec8c6189
wilians01 <wilians01100@gmail.com>
2025-01-26 17:31:47 -0300

    Criação do arquivo OutroExemDeHigher e exemplos

--- Estudos/src/estudos/dio/funcoes/tiposDeFuncoes/OutroExemDeHigher.kt
+++ Estudos/src/estudos/dio/funcoes/tiposDeFuncoes/OutroExemDeHigher.kt
@@ -1,2 +1,19 @@
 package estudos.dio.funcoes.tiposDeFuncoes
+//Retornando funções
 
+//1-Declara uma função de ordem superior que retorna uma função. (Int) -> Intrepresenta os parâmetros e o tipo de retorno da square função.
+//2-Declara uma função correspondente à assinatura.
+//3-Invoca operation para obter o resultado atribuído a uma variável. Aqui func se torna square que é retornado por operation.
+//4-Invoca func. A square função é realmente executada.
+
+
+fun operation(): (Int) -> Int {                                     // 1
+    return ::square
+}
+
+fun square(x: Int) = x * x                                          // 2
+
+fun main() {
+    val func = operation()                                          // 3
+    println(func(2))                                                // 4
+}
