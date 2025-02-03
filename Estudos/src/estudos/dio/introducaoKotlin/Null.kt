dfc17af8589ab931ba0d2995bda51669de2f8b12
wilians01 <wilians01100@gmail.com>
2025-02-03 17:57:54 -0300

    Atualizações

--- Estudos/src/estudos/dio/introducaoKotlin/Null.kt
+++ Estudos/src/estudos/dio/introducaoKotlin/Null.kt
@@ -2,7 +2,7 @@ package estudos.dio.introducaoKotlin
 
 fun main() {
     //caso queira que a sua variavl aceite valores nulll você tera que colocar um ponto de ?
-    //var exempço :String? = null// você não declara dessa maneira isso ira gerar um erro
+    //var exempço :String? = null// você não declara dessa maneira isso ira gerar um erro.
 
     fun tamanhoString(tamanhoDaString: String?): String {              // 1 Declaração da função
         if (tamanhoDaString != null && tamanhoDaString.length > 0) {
